5e5b199b9646a1f0b6f39e652c146d9a6c8955f9
Prasanna Sahu <prasannasahu4u@gmail.com>
2021-04-20 20:02:24 +0530

    commit

--- src/main/java/com/heapsteep/Microservice1Application.java
+++ src/main/java/com/heapsteep/Microservice1Application.java
@@ -2,6 +2,7 @@ package com.heapsteep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 public class Microservice1Application {
