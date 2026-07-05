58fd909e000ee90922d5658322f21571a3f7c8a0
prasanna-sahu <prasanna.sahu@mindnerves>
2026-07-05 11:11:58 +0530

    distributed tracing changes

--- src/main/java/com/heapsteep/controller/GreetingController.java
+++ src/main/java/com/heapsteep/controller/GreetingController.java
@@ -1,6 +1,8 @@
 package com.heapsteep.controller;
 
 import com.heapsteep.model.GreetingResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -11,11 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/greetings")
 public class GreetingController {
 
+	private static final Logger log = LoggerFactory.getLogger(GreetingController.class);
+
 	@Value("${server.port}")
 	private String port;
 
 	@GetMapping("/{name}")
 	public GreetingResponse getGreeting(@PathVariable String name) {
+		log.info("Received greeting request for name={}", name);
 		return new GreetingResponse(name, "Hello " + name + ", response from microservice-1 from port: " + port);
 	}
 
